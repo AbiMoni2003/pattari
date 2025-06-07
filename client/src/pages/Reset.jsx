38d7cbbe4f08e17109e3000cfe50df260b254acd
Abishek A <126270342+AbiMoni2003@users.noreply.github.com>
2025-06-07 14:01:46 +0530

    add

--- client/src/pages/Reset.jsx
+++ client/src/pages/Reset.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react'
 import { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
-import Footer from './Footer';
+
 
 
 function Reset() {
