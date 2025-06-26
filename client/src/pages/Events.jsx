ee7ca888f27c7713b23ae9c92b3765f604531c38
Abishek A <126270342+AbiMoni2003@users.noreply.github.com>
2025-06-26 11:49:11 +0530

    add

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -7,13 +7,8 @@ function Events() {
     <div>
         <Toggle/>
         <h1 className="events-heading">Events</h1>
-        <div className="events-div">
-            <h1 style={{fontSize:"18px"}}>படைப்பாற்றல்</h1>
-        <iframe width="400px" height="400px"
-        src='../../public/gallary/padaipattal.mp4'
-        className='padaipattal-video'
-      ></iframe>
-        </div>
+        
+        
         <Footer/>
     </div>
   )
