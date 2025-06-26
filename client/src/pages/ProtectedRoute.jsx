2ae1e3483f2893074aa1ab6937d777dbdd34bd13
Abishek A <126270342+AbiMoni2003@users.noreply.github.com>
2025-06-26 11:44:59 +0530

    add

--- client/src/pages/ProtectedRoute.jsx
+++ client/src/pages/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
 
-  if (isLoggedIn) {
+  if (!isLoggedIn) {
     return <Navigate to="/login" replace />;
   }
 
